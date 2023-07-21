319c86df78ca3dece52ad43b02a5b9c5c54f9ee4
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-07-21 10:45:06 +0400

    create vacancies list section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 import { HeroComponent } from './hero/hero.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent, HeroComponent],
-  imports: [RouterModule, HomeRoutingModule, MatIconModule],
+  imports: [RouterModule, HomeRoutingModule, MatIconModule, SharedModule],
 })
 export class HomeModule {}
