72f34e4f94a491eefa7745b9c18e44e3704c0342
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-07-22 15:26:37 +0400

    feat: add animations to the filters dropdown block and trigger arrow

--- src/app/vacancies/vacancies-filter/vacancies-filter-block/vacancies-filter-block.component.ts
+++ src/app/vacancies/vacancies-filter/vacancies-filter-block/vacancies-filter-block.component.ts
@@ -1,9 +1,27 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-vacancies-filter-block',
   templateUrl: './vacancies-filter-block.component.html',
   styleUrls: ['./vacancies-filter-block.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void=>*', [style({ opacity: 0 }), animate(300)]),
+      transition('*=>void', [animate(200, style({ opacity: 0 }))]),
+    ]),
+    trigger('openClose', [
+      state('open', style({ rotate: '180deg' })),
+      transition('open => close', [animate('0.25s')]),
+      transition('close => open', [animate('0.25s')]),
+    ]),
+  ],
 })
 export class VacanciesFilterBlockComponent {
   dropdownBlockIsShown = false;
--- src/app/vacancies/vacancies.module.ts
+++ src/app/vacancies/vacancies.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '../shared/shared.module';
 import { VacanciesFilterBlockComponent } from './vacancies-filter/vacancies-filter-block/vacancies-filter-block.component';
 import { VacanciesFilterComponent } from './vacancies-filter/vacancies-filter.component';
