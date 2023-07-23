003bbc77772b5e800a63198a903e363d25417073
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-07-23 14:17:38 +0400

    styles: customize the scrollbar

--- src/app/vacancies/vacancies.module.ts
+++ src/app/vacancies/vacancies.module.ts
@@ -1,15 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '../shared/shared.module';
 import { VacanciesFilterBlockComponent } from './vacancies-filter/vacancies-filter-block/vacancies-filter-block.component';
+import { VacanciesFilterOptionComponent } from './vacancies-filter/vacancies-filter-option/vacancies-filter-option.component';
 import { VacanciesFilterComponent } from './vacancies-filter/vacancies-filter.component';
+import { VacanciesFilterBarComponent } from './vacancies-filter/vacancies-filter-bar/vacancies-filter-bar.component';
 import { VacanciesListComponent } from './vacancies-list/vacancies-list.component';
 import { VacanciesRoutingModule } from './vacancies-routing.module';
 import { VacanciesComponent } from './vacancies.component';
-import { VacanciesFilterOptionComponent } from './vacancies-filter/vacancies-filter-option/vacancies-filter-option.component';
-import { VacanciesFilterBarComponent } from './vacancies-filter/vacancies-filter-bar/vacancies-filter-bar.component';
 
 @NgModule({
   declarations: [
