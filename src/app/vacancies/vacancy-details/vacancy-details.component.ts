084081913c8b4496c9ecc75f616b42980a4c5c75
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-07-31 13:13:11 +0400

    feat: show dynamic data on the vacancy detail page

--- src/app/vacancies/vacancy-details/vacancy-details.component.ts
+++ src/app/vacancies/vacancy-details/vacancy-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/store/app.reducer';
+import { vacanciesList } from '../store/vacancies.selectors';
+import { Vacancy } from '../vacancies.types';
 
 @Component({
   selector: 'app-vacancy-details',
   templateUrl: './vacancy-details.component.html',
-  styleUrls: ['./vacancy-details.component.scss']
+  styleUrls: ['./vacancy-details.component.scss'],
 })
-export class VacancyDetailsComponent {
+export class VacancyDetailsComponent implements OnInit, OnDestroy {
+  vacancyId: string;
+  storeSub: Subscription;
+  vacancy: Vacancy;
 
+  constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.vacancyId = this.route.snapshot.params['vacancyId'];
+    this.store.select(vacanciesList).subscribe((vacancies) => {
+      this.vacancy = vacancies.find((vacancy) => {
+        return vacancy.id === this.vacancyId;
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.storeSub.unsubscribe();
+  }
 }
