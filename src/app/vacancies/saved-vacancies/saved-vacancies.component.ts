ac67ee82d2b63bccd36fef41c84cff2125631ef3
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-08-14 13:56:45 +0400

    chore: unsubscribe store subscription in the saved vacancies component

--- src/app/vacancies/saved-vacancies/saved-vacancies.component.ts
+++ src/app/vacancies/saved-vacancies/saved-vacancies.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app.reducer';
 import { vacancies } from '../store/vacancies.selectors';
 import { Vacancy } from '../vacancies.types';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-saved-vacancies',
   templateUrl: './saved-vacancies.component.html',
   styleUrls: ['./saved-vacancies.component.scss'],
 })
-export class SavedVacanciesComponent implements OnInit {
+export class SavedVacanciesComponent implements OnInit, OnDestroy {
   savedVacancies: Vacancy[] = [];
   isLoading = false;
   error: string;
+  storeSub: Subscription;
 
   constructor(private store: Store<AppState>) {}
 
@@ -22,5 +24,9 @@ export class SavedVacanciesComponent implements OnInit {
       this.isLoading = vacanciesState.savedVacanciesLoading;
       this.error = vacanciesState.savedVacanciesError;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.storeSub.unsubscribe();
   }
 }
